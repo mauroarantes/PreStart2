391bcebf5e6b66b30b80268b9f67ac12f518464c
Mauro Arantes <mauro.arantes@gmail.com>
2022-02-07 13:06:06 +0000

    initial commit

---
+++ PreStart2/ContentView.swift
@@ -0,0 +1,66 @@
+//
+//  ContentView.swift
+//  PreStart2
+//
+//  Created by Mauro Arantes on 07/02/2022.
+//
+
+import SwiftUI
+
+struct Course: Hashable, Codable {
+    let name: String
+    let image: String
+}
+
+class ViewModel: ObservableObject {
+    
+    @Published var courses : [Course] = []
+    
+    func fetch() {
+        guard let url = URL(string: "https://iosacademy.io/api/v1/courses/index.php") else { return }
+        let task = URLSession.shared.dataTask(with: url) {[weak self] data, _, error in
+            guard let data = data, error == nil else { return }
+            do {
+                let courses = try JSONDecoder().decode([Course].self, from: data)
+                DispatchQueue.main.async {
+                    self?.courses = courses
+                }
+            } catch {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+}
+
+struct ContentView: View {
+    
+    @StateObject var viewModel = ViewModel()
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(viewModel.courses, id:\.self) { course in
+                    HStack {
+                        Image("")
+                            .frame(width: 130, height: 70)
+                            .background(Color.gray)
+                        Text(course.name)
+                            .bold()
+                    }
+                    .padding(3)
+                }
+            }
+            .navigationTitle("Courses")
+            .onAppear {
+                viewModel.fetch()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PreStart2/PreStart2App.swift
@@ -0,0 +1,17 @@
+//
+//  PreStart2App.swift
+//  PreStart2
+//
+//  Created by Mauro Arantes on 07/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct PreStart2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
